1c78be64652259436661590dfb59f77d3b45200f
GHost-code123 <eelhasni.enaa@gmail.com>
2024-12-29 11:03:00 +0100

    Add (404 not found) response

--- routes/task.js
+++ routes/task.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const Task = require('../models/task');
+const task = require('../models/task');
 
 const router = express.Router();
 
-
-//Post Method
 router.post('/tasks', async (req, res) => {
     const task = new Task({
         title: req.body.title,
@@ -13,11 +12,16 @@ router.post('/tasks', async (req, res) => {
 
     try {
         const dataToSave = await task.save();
-        res.status(200).json(dataToSave)
+        res.status(201).json(dataToSave)
     }
     catch (error) {
         res.status(400).json({ message: error.message })
     }
+});
+
+router.all('*', (req, res) => {
+	res.sendStatus(404);
+	res.end();
 });
 
 module.exports = router;
