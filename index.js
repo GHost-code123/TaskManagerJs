e71a988a0a7840ce8bf19d14dc82e07537ee842d
GHost-code123 <eelhasni.enaa@gmail.com>
2024-12-29 10:31:00 +0100

    Connect MongoDB database

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// ------ Mongodb connection ------ //
+
+const mongodbURI = 'mongodb://localhost:27017/tasks'
+
+mongoose.connect(mongodbURI);
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+    console.log(error)
+});
+
+db.once('connected', () => {
+    console.log('Database Connected');
+});
+
+ 
 // ------ Set up Server ------ //
 
 const port = 3000;
